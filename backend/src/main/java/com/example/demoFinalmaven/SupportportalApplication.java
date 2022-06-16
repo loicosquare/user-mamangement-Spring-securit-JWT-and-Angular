7b271a061f54c06373d7861a381dfe0258380976
loico square <loicsan07@gmail.com>
2022-06-16 20:30:55 +0100

    initialize backend

---
+++ backend/src/main/java/com/example/demoFinalmaven/SupportportalApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoFinalmaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupportportalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupportportalApplication.class, args);
+	}
+
+}
---
+++ backend/src/main/java/com/example/demoFinalmaven/domain/User.java
@@ -0,0 +1,6 @@
+package com.example.demoFinalmaven.domain;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+}
