3d86f95819392e2a37d5d861e4c087ef70d37ab0
loico square <loicsan07@gmail.com>
2022-06-17 07:51:44 +0100

    JWT Token provider PART 2.

--- backend/src/main/java/com/example/demoFinalmaven/utility/JWTTokenProvider.java
+++ backend/src/main/java/com/example/demoFinalmaven/utility/JWTTokenProvider.java
@@ -1,29 +1,67 @@
 package com.example.demoFinalmaven.utility;
 
 import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.example.demoFinalmaven.constant.SecurityConstant;
 import com.example.demoFinalmaven.domain.UserPrincipal;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.DoubleStream;
 
 import static com.auth0.jwt.algorithms.Algorithm.HMAC512;
+import static com.example.demoFinalmaven.constant.SecurityConstant.*;
+import static java.util.stream.StreamSupport.stream;
 
 public class JWTTokenProvider {
-    @Value("jwt.secret")
+    @Value("${jwt.secret}")
     private String secret;
 
-    //This method is responsible to Generate the Token.
+    //This method is responsible to Generate the Token after the user has been authenticated.
     public String generateJwtToken(UserPrincipal userPrincipal){
         String[] claims = getClaimsFromUser(userPrincipal);
         return JWT.create().withIssuer(SecurityConstant.LOIC_SANOU_LLC).withAudience(SecurityConstant.LOIC_SANOU_ADMINISTRATION)
                 .withIssuedAt(new Date()).withSubject(userPrincipal.getUsername())
-                .withArrayClaim(SecurityConstant.AUTHORITIES, claims)
+                .withArrayClaim(AUTHORITIES, claims)
                 .withExpiresAt(new Date(System.currentTimeMillis() + SecurityConstant.EXPIRATION_TIME))
                 .sign(HMAC512(secret.getBytes()));
     }
 
-    private String[] getClaimsFromUser(UserPrincipal userPrincipal) {
+    public List<GrantedAuthority> getAuthorities(String token){
+        String[] claims = getClaimsFromToken(token);
+        return stream(claims).map(SimpleGrantedAuthority::new).collect(Collectors.toList());
+    }
+
+    private String[] getClaimsFromToken(String token) {
+        JWTVerifier verifier = getJWTVerifier();
+        return verifier.verify(token).getClaim(AUTHORITIES).asArray(String.class);
+    }
+
+    //Verify the token with the claims that w've provided
+    private JWTVerifier getJWTVerifier() {
+        JWTVerifier verifier;
+        try{
+            Algorithm algorithm = HMAC512(secret);
+            verifier = JWT.require(algorithm).withIssuer(LOIC_SANOU_LLC).build();
+        }catch(JWTVerificationException exception){
+            throw new JWTVerificationException(ACCES_DENIED_MESSAGE);
+        }
+        return verifier;
+    }
+
+    private String[] getClaimsFromUser(UserPrincipal user) {
+        List<String> authorities = new ArrayList<>();
+        for (GrantedAuthority grantedAuthority : user.getAuthorities()){
+            authorities.add(grantedAuthority.getAuthority());
+        }
+        return  authorities.toArray(new String[0]);
     }
 
 }
